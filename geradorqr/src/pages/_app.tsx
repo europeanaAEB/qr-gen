feeb8e6561c695c27bc8bf21194fbb8265167a6c
Thiago <74683757+ThiagoSantosOFC@users.noreply.github.com>
2023-03-14 11:05:16 +0000

    :sparkles: `FEAT` NavBar QRGEN inicio #2

--- geradorqr/src/pages/_app.tsx
+++ geradorqr/src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import NavBar from "../../components/NavBar";
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>Gerador QR</title>
+        <meta name="description" content="Gerador de codigo QR gratuito" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/qrgen.svg " />
+      </Head>
+      <NavBar />
+      <Component {...pageProps} />
+    </div>
+  );
 }
